e88692f71bc0bd86a390b0da1c1e6271dab599ad
cslsds <lambdadatasciencelearning@gmail.com>
2020-07-13 18:30:46 -0400

    pushing mod1 completed project

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,59 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+## Python list/array implementation:::
+
+# class Queue:
+#     # first in - first out 
+#     def __init__(self):
+#         self.size = 0
+#         # self.storage = ?
+#         self.storage = []
+    
+#     def __len__(self):
+#         # returns num of elements in the queue
+#         return self.size
+
+#     def enqueue(self, value):
+#         # adds an element to the back of the queue
+#         self.storage.append(value)
+#         self.size += 1
+
+#     def dequeue(self):
+#         # removes and returns the element at the front of the queue
+#         if self.size > 0:
+#             self.size -= 1
+#            return self.storage.pop(0)
+
+
+
+# LinkedList() implementation
+from singly_linked_list import Node, LinkedList
+
 class Queue:
+    # first in - first out 
     def __init__(self):
         self.size = 0
         # self.storage = ?
+        self.storage = LinkedList()
     
     def __len__(self):
-        pass
+        # returns num of elements in the queue
+        return self.size
 
     def enqueue(self, value):
-        pass
+        # adds an element to the back of the queue
+        
+        # initialize instance of storage/LinkedList();
+        #     call 'add_to_tail' method to enqueue 'value' defined in params
+        self.storage.add_to_tail(value)
+        self.size += 1
 
     def dequeue(self):
-        pass
+        # removes and returns the element at the front of the queue
+
+        # initialize instance of 
+        if self.size > 0:
+            self.size -= 1
+        return self.storage.remove_head()
--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,51 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+# # Python list/array implementation
+
+# class Stack:
+#     # first in - last out
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         # returns num of elements in the stack
+#         return self.size
+
+#     def push(self, value):
+#         # adds item to the top of the stack
+#         self.storage.append(value)
+#         self.size += 1
+
+#     def pop(self):
+#         # removes and returns element at top of stack
+#         if self.size > 0:
+#             self.size -= 1
+#             return self.storage.pop()
+
+# LinkedList implementation
+
+from singly_linked_list import Node, LinkedList
+
 class Stack:
+    # first in - last out
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
+        # returns num of elements in the stack
+        return self.size
 
     def push(self, value):
-        pass
+        # adds item to the top of the stack
+        self.storage.add_to_tail(value)
+        self.size += 1
 
     def pop(self):
-        pass
+        # removes and returns element at top of stack
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_tail()
